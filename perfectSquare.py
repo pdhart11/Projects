103f0ba899dfcbc83cc26dc2934b0633a9f1e82e
pdhart11 <106359270+pdhart11@users.noreply.github.com>
2022-08-25 12:42:57 -0400

    Create perfectSquare.py

---
+++ perfectSquare.py
@@ -0,0 +1,29 @@
+import math
+value = input("Enter N: ")
+n_array = []
+i = 1
+while i <= int(value):
+    n_array.append(i)
+    i = i + 1
+
+new_array = [math.ceil(((int(value))+1)/2)]
+n_array.pop(new_array[0]-1)
+k = 0
+counter = 0
+while k < int(value) -1:
+    counter += 1
+    if counter > int(value):
+        print("No solution found")
+        print("Remaining numbers: " + str(n_array))
+        break
+
+    for j in range(len(n_array)-1, -1, -1):
+        
+        if (math.sqrt(new_array[k] + n_array[j])) - math.floor(math.sqrt(new_array[k] + n_array[j])) == 0:
+            
+            new_array.append(n_array[j])
+            n_array.pop(j)
+            k+=1
+            break
+
+print("Final result: " + str(new_array))
